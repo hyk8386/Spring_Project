499db7603fc15f8046a0d97561c6985d6bda70b2
DESKTOP-DLCB8GM\505 <lkw8342@gmail.com>
2022-08-23 16:38:56 +0900

    0823 CSS 적용

---
+++ bookShop01/src/main/java/com/bookshop01/cart/controller/CartControllerImpl.java
@@ -0,0 +1,104 @@
+package com.bookshop01.cart.controller;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookshop01.cart.service.CartService;
+import com.bookshop01.cart.vo.CartVO;
+import com.bookshop01.common.base.BaseController;
+import com.bookshop01.goods.vo.GoodsVO;
+import com.bookshop01.member.vo.MemberVO;
+
+@Controller("cartController")	// 해당 클래스가 Controller임을 나타내기 위한 어노테이션
+@RequestMapping(value="/cart")	// 요청에 대해 어떤 Controller, 어떤 메소드가 처리할지를 맵핑하기 위한 어노테이션
+public class CartControllerImpl extends BaseController implements CartController{
+	@Autowired
+	CartService cartService;
+	@Autowired
+	CartVO cartVO;
+	@Autowired
+	MemberVO memberVO;
+	
+	/// myCartList.do 요청을 GET 방식으로 받음
+	@RequestMapping(value="/myCartList.do" ,method = RequestMethod.GET)
+	// ModelAndView -> 데이터와 뷰를 동시에 설정 / 메서드에서 생성해서 데이터를 담은 후 돌려준다
+	public ModelAndView myCartMain(HttpServletRequest request, HttpServletResponse response)  throws Exception {
+		String viewName=(String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView(viewName);
+		HttpSession session=request.getSession();
+		MemberVO memberVO=(MemberVO)session.getAttribute("memberInfo");
+		String member_id=memberVO.getMember_id();
+		cartVO.setMember_id(member_id);
+		Map<String ,List> cartMap=cartService.myCartList(cartVO);	// 장바구니 페이지에 표시할 상품 정보를 조회
+		session.setAttribute("cartMap", cartMap);//장바구니 목록 화면에서 상품 주문 시 사용하기 위해서 장바구니 목록을 세션에 저장한다.
+		//mav.addObject("cartMap", cartMap);
+		return mav;
+	}
+	
+	// /addGoodsInCart.do를 요청하면 addGoodsInCart메서드가 호출됨
+	@RequestMapping(value="/addGoodsInCart.do" ,method = RequestMethod.POST,produces = "application/text; charset=utf8")
+	public  @ResponseBody String addGoodsInCart(@RequestParam("goods_id") int goods_id, // 전송된 상품번호를 받음
+			                    HttpServletRequest request, HttpServletResponse response)  throws Exception{
+		HttpSession session=request.getSession();
+		memberVO=(MemberVO)session.getAttribute("memberInfo");
+		String member_id=memberVO.getMember_id();
+		
+		cartVO.setMember_id(member_id);
+		//카트 등록전에 이미 등록된 제품인지 판별한다.
+		cartVO.setGoods_id(goods_id);
+		cartVO.setMember_id(member_id);
+		boolean isAreadyExisted=cartService.findCartGoods(cartVO);
+		System.out.println("isAreadyExisted:"+isAreadyExisted);
+		// 상품번호가 이미 장바구니 테이블에 있으면 이미 추가되었다는 메시지를 브라우저로 전송하고, 없으면 장바구니 테이블에 추가함
+		if(isAreadyExisted==true){
+			return "already_existed";
+		}else{
+			cartService.addGoodsInCart(cartVO);
+			return "add_success";
+		}
+	}
+	
+	@RequestMapping(value="/modifyCartQty.do" ,method = RequestMethod.POST)
+	public @ResponseBody String  modifyCartQty(@RequestParam("goods_id") int goods_id,
+			                                   @RequestParam("cart_goods_qty") int cart_goods_qty,
+			                                    HttpServletRequest request, HttpServletResponse response)  throws Exception{
+		HttpSession session=request.getSession();
+		memberVO=(MemberVO)session.getAttribute("memberInfo");
+		String member_id=memberVO.getMember_id();
+		cartVO.setGoods_id(goods_id);
+		cartVO.setMember_id(member_id);
+		cartVO.setCart_goods_qty(cart_goods_qty);
+		boolean result=cartService.modifyCartQty(cartVO);
+		
+		if(result==true){
+		   return "modify_success";
+		}else{
+			  return "modify_failed";	
+		}
+		
+	}
+	
+	@RequestMapping(value="/removeCartGoods.do" ,method = RequestMethod.POST)
+	public ModelAndView removeCartGoods(@RequestParam("cart_id") int cart_id,
+			                          HttpServletRequest request, HttpServletResponse response)  throws Exception{
+		ModelAndView mav=new ModelAndView();
+		cartService.removeCartGoods(cart_id);
+		mav.setViewName("redirect:/cart/myCartList.do");
+		return mav;
+	}
+}
