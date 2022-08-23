b8d5595f92b3e93fe9eca0f70c82b11b6610f78e
DESKTOP-DLCB8GM\505 <lkw8342@gmail.com>
2022-08-23 16:48:06 +0900

    수정

--- bookShop01/src/main/java/com/bookshop01/common/file/FileDownloadController.java
+++ bookShop01/src/main/java/com/bookshop01/common/file/FileDownloadController.java
@@ -17,7 +17,7 @@ import net.coobird.thumbnailator.Thumbnails;
 
 @Controller
 public class FileDownloadController {
-	private static String CURR_IMAGE_REPO_PATH = "C:\\shopping_camp\\file_repo";
+	private static String CURR_IMAGE_REPO_PATH = "C:\\shopping\\file_repo";
 	
 	@RequestMapping("/download")
 	protected void download(@RequestParam("fileName") String fileName,
@@ -32,8 +32,8 @@ public class FileDownloadController {
 		FileInputStream in=new FileInputStream(image); 
 		byte[] buffer=new byte[1024*8];
 		while(true){
-			int count=in.read(buffer); //ï¿½ï¿½ï¿½Û¿ï¿½ ï¿½Ð¾ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ú°ï¿½ï¿½ï¿½
-			if(count==-1)  //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ß´ï¿½ï¿½ï¿½ Ã¼Å©
+			int count=in.read(buffer); //¹öÆÛ¿¡ ÀÐ¾îµéÀÎ ¹®ÀÚ°³¼ö
+			if(count==-1)  //¹öÆÛÀÇ ¸¶Áö¸·¿¡ µµ´ÞÇß´ÂÁö Ã¼Å©
 				break;
 			out.write(buffer,0,count);
 		}
